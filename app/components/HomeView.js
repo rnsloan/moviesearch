fced5984ca95175f18bcf4a6dd11ff77684c4585
rnsloan <rnsloan1@gmail.com>
2015-09-14 03:31:09 +1000

    add search input onto homepage

--- app/components/HomeView.js
+++ app/components/HomeView.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class extends React.Component {
+  handleClick(e) {
+    e.preventDefault();
+    const searchQuery = this.refs.searchInput.value;
+
+  }
+
   render() {
     return (
       <div styleName='home'>
         <h1>Home</h1>
+        <input type="text" ref="searchInput" placeholder="e.g. mad max" />
+        <button onClick={(e) => this.handleClick(e)}>Search</button>
       </div>
     );
   }
