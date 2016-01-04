d6530b3c2a330017542513a3ad3a2b8e8cd61809
rnsloan <rnsloan1@gmail.com>
2016-01-04 14:52:00 +1100

    fix tests and bad export. Ignore correct public directory

--- app/utils/movieApi.js
+++ app/utils/movieApi.js
@@ -9,12 +9,11 @@ if (typeof __MOVIEAPIKEY__ === 'undefined') {
 
 const urlPath = '//api.themoviedb.org/3/';
 
-export default {
-  searchMovie: function(title) {
-    return axios.get(`${urlPath}search/movie?api_key=${__MOVIEAPIKEY__}&query=${title}`)
-  },
-  getMovie: function(id) {
-    return axios.get(`${urlPath}movie/${id}?api_key=${__MOVIEAPIKEY__}`)
-  },
-  rootBackdropPath: '//image.tmdb.org/t/p/w1280/'
+export function searchMovie(title) {
+  return axios.get(`${urlPath}search/movie?api_key=${__MOVIEAPIKEY__}&query=${title}`)
 }
+export function getMovie(id) {
+  return axios.get(`${urlPath}movie/${id}?api_key=${__MOVIEAPIKEY__}`)
+}
+
+export const rootBackdropPath = '//image.tmdb.org/t/p/w1280/';
